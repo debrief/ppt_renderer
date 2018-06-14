f6c2a5d8bb4fbf97fb4ddba5fab112e6612a0c8f
purvil12c <purvil12c@gmail.com>
2018-06-14 17:24:05 +0530

    seperated pack/unpack scripts, default parameters added and a few condition checks added

--- README.md
+++ README.md
@@ -4,8 +4,12 @@ Development project to produce body of scripts for manipulating PPT slide
 # unpack_pack script -
 To run the script -
 Unpack:
-python pack_unpack.py 1 Desktop/sample.pptx Desktop/unpack
+python scripts/unpack.py Desktop/sample.pptx Desktop/unpack
+python scripts/unpack.py --pptx_path Desktop/sample.pptx
 Pack:
-python pack_unpack.py 2 Desktop/sample.pptx Desktop/unpack
+python scripts/pack.py --pptx_path Desktop/sample.pptx --unpack_path Desktop/unpack
+python scripts/pack.py --unpack_path unpacked_pptx/
 
-Run Help - python pack_unpack.py -h
+Run Help -
+python pack.py -h
+python pack.py -h
---
+++ scripts/pack.py
@@ -0,0 +1,35 @@
+#Script for packing a pptx file
+#Sample run - python pack.py Desktop/sample.pptx Desktop/unpack
+import zipfile
+import argparse
+import os
+import shutil
+import sys
+
+parser = argparse.ArgumentParser(description='Script to pack a unpacked pptx file')
+parser.add_argument('--pptx_path', help='Path to destination packed pptx file', required=False)
+parser.add_argument('--unpack_path', help='Path to unpacked pptx folder', required=False)
+args = parser.parse_args()
+
+pptx_path = args.pptx_path
+unpack_path = args.unpack_path
+
+if(unpack_path == None):
+    print("Provide unpack_path (path to directory containing unpacked pptx)")
+    sys.exit(1)
+
+if(pptx_path==None):
+    if(unpack_path[-1]=="/" or unpack_path[-1]=="\\"):
+        pptx_path = unpack_path[0:-1]+".pptx"
+    else:
+        pptx_path = unpack_path[0:-1]+".pptx"
+
+#check if unpack_path is directory or not
+if(not os.path.isdir(unpack_path)):
+    print "unpack_path provided is not a directory"
+    sys.exit(1)
+
+#Pack the unpack_path folder to pptx_path pptx file
+shutil.make_archive(pptx_path, 'zip', unpack_path)
+os.rename(pptx_path+".zip", pptx_path)
+print("File packed at "+pptx_path)
