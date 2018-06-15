c1b810e7c4f2d0807472aecd239c3292a5177601
purvil12c <purvil12c@gmail.com>
2018-06-15 15:31:10 +0530

    refactored findmap.py to function

--- scripts/findmap.py
+++ scripts/findmap.py
@@ -1,15 +1,21 @@
 #Script for fetching details about a rectangle named "map" from the provided unpacked pptx
 #Run - python scripts/findmap.py --unpack_path map_rect
-import argparse
+#Use getMapDetails function
+# from findmap import getMapDetails
+#
+# print getMapDetails("map_rect")
+
+
+# import argparse
 from bs4 import BeautifulSoup
 import os
 
-parser = argparse.ArgumentParser(description='Script to find details about rectangle named map')
-parser.add_argument('--unpack_path', help='Path to unpacked pptx folder', required=True)
-args = parser.parse_args()
+# parser = argparse.ArgumentParser(description='Script to find details about rectangle named map')
+# parser.add_argument('--unpack_path', help='Path to unpacked pptx folder', required=True)
+# args = parser.parse_args()
 
-unpack_path = args.unpack_path
-slides_base_path = unpack_path+"/ppt/slides"
+# unpack_path = args.unpack_path
+# slides_base_path = unpack_path+"/ppt/slides"
 
 def getSlides(slides_base_path):
     temp = os.listdir(slides_base_path)
@@ -19,7 +25,7 @@ def getSlides(slides_base_path):
             slides_path.append(slides_base_path+"/"+slide)
     return slides_path
 
-slides_path = getSlides(slides_base_path)
+# slides_path = getSlides(slides_base_path)
 #Slides_path contains path to all the slide.xml files
 
 #Now we need to check for every slide if it contains a rectangle named map
@@ -29,7 +35,9 @@ def checkForMap(slides_path):
     for slidePath in slides_path:
         flag=0
         soup = BeautifulSoup(open(slidePath, 'r').read(), 'lxml')
+        # print "soup","\n\n\n", soup,"\n\n"
         shapes = soup.find_all("p:sp")
+        # print len(shapes)
         for shape in shapes:
             shapeDetails = {}
             for child in shape.find_all("p:cnvpr"):
@@ -40,8 +48,7 @@ def checkForMap(slides_path):
             for child in shape.find_all("a:ext"):
                 shapeDetails['cx'] = child['cx']
                 shapeDetails['cy'] = child['cy']
-
-            print "shapeDetails --", shapeDetails
+            # print "shapeDetails --", shapeDetails
             if(shapeDetails['name']=="map"):
                 mapDetails = shapeDetails
                 flag=1
@@ -51,10 +58,14 @@ def checkForMap(slides_path):
 
     return mapDetails
 
-mapDetail = checkForMap(slides_path)
-print "mapDetails - ",mapDetail
-
-if(not 'name' in mapDetail):
-    print "No shape named 'Map' found..."
-else:
-    print "Top Left Corner: x=",mapDetail['x']," y=",mapDetail['y']," \nExtentions: cx:",mapDetail['cx']," cy:",mapDetail['cy']
+def getMapDetails(unpack_path):
+    slides_base_path = unpack_path+"/ppt/slides"
+    slides_path = getSlides(slides_base_path)
+    mapDetail = checkForMap(slides_path)
+    return mapDetail
+# print "mapDetails - ",mapDetail
+#
+# if(not 'name' in mapDetail):
+#     print "No shape named 'Map' found..."
+# else:
+#     print "Top Left Corner: x=",mapDetail['x']," y=",mapDetail['y']," \nExtentions: cx:",mapDetail['cx']," cy:",mapDetail['cy']
