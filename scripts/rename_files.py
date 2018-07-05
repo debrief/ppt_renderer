965574e05a0627844f3946db6f7476869665efdd
purvil12c <purvil12c@gmail.com>
2018-07-05 14:11:06 +0530

    labeling images

--- scripts/rename_files.py
+++ scripts/rename_files.py
@@ -4,6 +4,10 @@ import shutil
 from datetime import datetime
 from datetime import timedelta
 
+from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw
+
 def renameFiles(src_path, skip_factor=1, first_file_name='19951212_093600'):
     skip_factor = int(skip_factor)
     #First filename const = 19951212_093600
@@ -35,7 +39,17 @@ def renameFiles(src_path, skip_factor=1, first_file_name='19951212_093600'):
     for root, dirs, files in os.walk(dest_path):
         files.sort()
         for f in files:
+
             file_name_str = file_name.strftime('%Y%m%d_%H%M%S')
+
+            date_time_strs = file_name_str.split('_')
+            img = Image.open(dest_path+'/'+f)
+            draw = ImageDraw.Draw(img)
+            font = ImageFont.truetype("Arial.ttf", 96)
+            draw.text((0, 0),date_time_strs[0],(255,255,255),font=font)
+            draw.text((950, 0),date_time_strs[1],(255,255,255),font=font)
+            img.save(dest_path+'/'+f)
+
             os.rename(dest_path+'/'+f, dest_path+ '/' + file_name_str+'.jpg')
             file_name = file_name + timedelta(seconds=20*skip_factor)
 
